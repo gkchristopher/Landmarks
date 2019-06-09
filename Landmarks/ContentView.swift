154dd863aa847eedd618417ca060303a8e3a5b23
Christopher Moore <gkchristopher@icloud.com>
2019-06-09 09:09:21 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,17 @@
+//Copyright © 2019 Roving Mobile. All rights reserved.
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
